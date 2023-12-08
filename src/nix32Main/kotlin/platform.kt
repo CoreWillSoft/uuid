9462382c4daba8cdfec78b57aeb265f3d40bccde
mkhytarmkhoian <mkhytar.mkhoian@gmail.com>
2023-12-08 10:20:29 +0200

    Remove all targets except android and IOS

--- src/nix32Main/kotlin/platform.kt
+++
@@ -1,9 +0,0 @@
-package com.benasher44.uuid
-
-import platform.posix.read
-
-internal actual fun getRandomUuidBytes(): ByteArray {
-    return bytesWithURandomFd { fd, bytePtr ->
-        read(fd, bytePtr, UUID_BYTES.toUInt())
-    }
-}
